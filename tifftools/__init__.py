0fe8f42479ad784663d6f8b8788c2f0a66f372ef
David Manthey <david.manthey@kitware.com>
2020-09-30 12:09:47 -0400

    Show the version as part of help.

--- tifftools/__init__.py
+++ tifftools/__init__.py
@@ -1,9 +1,18 @@
 import logging
 
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .commands import main, tiff_concat, tiff_dump, tiff_info, tiff_merge, tiff_set, tiff_split
 from .constants import Datatype, Tag, TiffDatatype, TiffTag
 from .exceptions import MustBeBigTiffException, TifftoolsException, UnknownTagException
 from .tifftools import read_tiff, write_tiff
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'development'
+
 
 logger = logging.getLogger(__name__)
 
@@ -28,5 +37,6 @@ __all__ = (
     'tiff_set',
     'tiff_split',
 
+    '__version__',
     'main',
 )
