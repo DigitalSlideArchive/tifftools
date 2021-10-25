528b348f3481eaa616336051a77d35a32f3a08c2
David Manthey <david.manthey@kitware.com>
2021-10-25 10:33:11 -0400

    Speed up the constants class slightly.
    This is based on profiling in Python 3.8 of tiff files with huge numbers
    of directories.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -30,17 +30,33 @@ def test_tiffconstantset():
     assert 270 in s
     assert 'ImageDescription' in s
     assert 'imagedescription' in s
+    assert 'IMAGEDESCRIPTION' in s
     assert 'notpresent' not in s
     assert s[270].name == 'ImageDescription'
+    assert s['270'].name == 'ImageDescription'
     assert '270' in s
     assert '0x10e' in s
     assert s.ImageDescription == 270
+    assert s.imagedescription == 270
+    assert s.IMAGEDESCRIPTION == 270
+    assert s['ImageDescription'] == 270
+    assert s['imagedescription'] == 270
+    assert s['IMAGEDESCRIPTION'] == 270
     assert getattr(s, '270').name == 'ImageDescription'
     assert getattr(s, '0x10e').name == 'ImageDescription'
     with pytest.raises(KeyError):
         s['notpresent']
     assert s.get('ImageDescription').name == 'ImageDescription'
     assert s.get('notpresent') is None
+
+
+def test_tiffconstant_property():
+    c = tifftools.Tag.ImageDescription
+    assert c.isIFD() is False
+    assert not isinstance(c.datatype, tuple)
+    c = tifftools.Tag.SubIFD
+    assert c.isIFD() is True
+    assert isinstance(c.datatype, tuple)
 
 
 def test_get_or_create_tag():
